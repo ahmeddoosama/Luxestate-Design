b8a1ac3f2046112a5d4e98429ef285d7511e974a
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-02-26 18:04:41 +0200

    add funtcion counter number for about section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
+//Function for add box-shadow on navbar if scroll is greater||equal then 100
 $(function () {
     var hasBeenTrigged = false;
     $(window).scroll(function () {
-      if ($(this).scrollTop() >= 100 && !hasBeenTrigged) { // if scroll is greater/equal then 100 and hasBeenTrigged is set to false.
-        $('.navbar').css("box-shadow", "0px 10px 15px 0px rgb(29 31 36 / 10%)");
-        hasBeenTrigged = true;
-      }else if ($(this).scrollTop() <= 100 && hasBeenTrigged) {
-        $('.navbar').css("box-shadow", "none");
-        hasBeenTrigged = false;
-      }
+        if ($(this).scrollTop() >= 100 && !hasBeenTrigged) {
+            $('.navbar').css("box-shadow", "0px 10px 15px 0px rgb(29 31 36 / 10%)");
+            hasBeenTrigged = true;
+        } else if ($(this).scrollTop() <= 100 && hasBeenTrigged) {
+            $('.navbar').css("box-shadow", "none");
+            hasBeenTrigged = false;
+        }
     });
-  });
+});
+
+//counter number
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
